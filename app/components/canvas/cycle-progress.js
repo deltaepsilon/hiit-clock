2b44ef51b6d56788cf0fba8e2f7485e9da9da598
Chris Esplin <chris@christopheresplin.com>
2019-04-26 07:25:05 -0600

    Updating gitlab config

--- app/components/canvas/cycle-progress.js
+++ app/components/canvas/cycle-progress.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default ({ timer, cycles, secondsElapsed }) => {
   return (
     <>
-      <h1>Page Header</h1>
+      <h1>{timer.name}</h1>
+      <p>{cycles.length}</p>
+      <span>{secondsElapsed}</span>
     </>
   );
 };
