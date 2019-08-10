c776ab1e201fa7fcc60b903699cf1f97e40b4c23
Chris Esplin <chris@christopheresplin.com>
2019-08-10 04:47:26 -0600

    Another attempt at disabling zoom

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -9,7 +9,7 @@ export default class CustomDocument extends Document {
           <meta charSet="UTF-8" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
           />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link rel="icon" href="/images/favicon.ico" />
