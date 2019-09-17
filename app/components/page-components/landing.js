2831757a1194e5745e563fcb8a958bf797bf340a
Chris Esplin <chris@christopheresplin.com>
2019-09-17 07:10:14 -0600

    YouTube landing page embed

--- app/components/page-components/landing.js
+++ app/components/page-components/landing.js
@@ -84,6 +84,17 @@ export default () => {
           </List>
         </section>
 
+        <section>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/gaKfMSWPAwQ"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </section>
+
         <hr />
 
         <section>
